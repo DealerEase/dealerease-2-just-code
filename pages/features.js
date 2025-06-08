2f385a8698f5466ab5410e4b28f0b429e501a76b
DealerEase <dealerease1@gmail.com>
2025-06-07 22:49:56 -0700

    Update features.js

--- pages/features.js
+++ pages/features.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import Header from "../components/Header";
+import Header from "../components/Header.js";
 
 export default function FeaturesPage() {
   const [selected, setSelected] = useState([]);
